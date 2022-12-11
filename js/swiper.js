bf8d3025251039765b2f7918a5d5219d2b038613
ywiio <maschkantseva.zlata@yandex.ru>
2022-12-11 14:32:46 +0300

    add swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,74 +1,33 @@
-const menu = document.querySelector('#menu-bars'),
-      navbar = document.querySelector('.navbar'),
-      themeToggler = document.querySelector('.theme-toggler'),
-      toggleBtn = document.querySelector('.toggle-btn');
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+  scrollbar: {
+    el:'.swiper-slide',
+    draggable:true,
+  },
 
-menu.onclick = () => {
-  menu.classList.toggle('fa-times')
-  navbar.classList.toggle('active')
-}
+  grabCursor:true,
+//???????
+  slideToClickedSlide:true,
+  
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 
-window.onscroll = () => {
-  menu.classList.remove('fa-times')
-  navbar.classList.remove('active')
-  themeToggler.classList.remove('active')
-}
+  // mousewheel:{
+  //   sensitivity:1,
+  // },
 
-toggleBtn.onclick = () => {
-  themeToggler.classList.toggle('active')
-}
+  slidesPerView:3.5,
+  watchOverflow:true, //если у слайдера меньше слайдов чем нужно отобразить
+ 
+  spaceBetween:30,
 
-
-document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn => {
-  btn.onclick = () => {
-    let color = btn.style.backgroundColor;
-    document.querySelector(':root').style.setProperty('--main-color', color);
-  }
-}) 
-
-import Swiper from 'swiper-bundle.esm.js';
-// import 'swiper-bundle.css';
-//Sliders
-
-const swiper = new Swiper(".team__cards", {
-      direction: 'horizontal',
-      loop: true,
-      effect: "coverflow",
-      slidesPerView: 4,
-      centeredSlides: true,
-      spaceBetween: 30,
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-        // coverflowEffect: {
-        //   rotate: 50,
-        //   stretch: 0,
-        //   depth: 100,
-        //   modifier: 1, 
-        //   slideShadows: true, 
-        // },
-        // pagination: {
-        //   el: ".swiper-pagination",
-        // },
+  centeredSlides:true,
 });
-
-// const slider = new Swiper(".review-slider", {
-//         effect: "coverflow",
-//         grabCursor: true,
-//         centeredSlides: true,
-//         slidesPerView: "auto",
-//         coverflowEffect: {
-//           rotate: 50,
-//           stretch: 0,
-//           depth: 100,
-//           modifier: 1, 
-//           slideShadows: true, 
-//         },
-//         autoplay: {
-//           delay: 3000,
-//           disableOnInteraction: false
-//         },
-//         pagination: {
-//           el: ".swiper-pagination",
-//         },
-// }); 
